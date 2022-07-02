33d59eb40fc13ed60daef9eedadbf4b577e548d9
Chiamaka Anowai <chiamakaanowai@gmail.com>
2022-07-02 15:00:55 -0700

    4-puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,20 @@
-#!/bin/bash
 #include <stdio.h>
 
-/
 
- * main - Entry point
- *
- *  * Return: Always 0 (sucess)
- *
- *   */
 
- int main(void)
+/**
+ 
+ *	main - Prints a string with puts
+ *
+ *	Return: 0 if program executed successfully
+ */
+
+int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
